f8d4fe3f1408254f9345657789074576b971d2b7
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-09-14 16:06:24 -0300

    Parte do requisito 3 concluído

---
+++ script.js
@@ -0,0 +1,13 @@
+function checarDados() {
+    const email = document.getElementById('login-email').value;
+    const password = document.getElementById('login-password').value;
+
+    if(email === 'tryber@teste.com' && password === "123456") {
+        alert("Olá, Tryber!");
+    } else {
+        alert("Email ou senha inválidos.");
+    }
+}
+
+const enviar = document.getElementById("login-enviar");
+enviar.addEventListener("click", checarDados);

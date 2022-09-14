9787ab947b0588ae20c36dfaf5cc2bf1df7f77f3
wesleyRenam <wesleyrenamjob@gmail.com>
2022-09-14 16:28:14 -0300

    desafio 7

--- script.js
+++ script.js
@@ -2,19 +2,11 @@ function checarDados() {
   const email = document.getElementById('login-email').value;
   const password = document.getElementById('login-password').value;
 
-<<<<<<< HEAD
   if (email === 'tryber@teste.com' && password === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
   }
-=======
-    if(email === 'tryber@teste.com' && password === '123456') {
-        alert('Olá, Tryber!');
-    } else {
-        alert('Email ou senha inválidos.');
-    }
->>>>>>> 19541f360903826441c37158975a4d5070023bcb
 }
 
 const enviar = document.getElementById('login-enviar');
